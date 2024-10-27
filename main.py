f8904a3230d983846663553a3701946098c56dd2
jmzion7 <jmwatt@proton.me>
2024-10-27 15:36:58 -0700

    B: additional game mechanics

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     
@@ -14,8 +28,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        for obj in updatable:
+            obj.update(dt)
+        
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            
         screen.fill((0,0,0))
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         #limit the framerate to 60 FPS
